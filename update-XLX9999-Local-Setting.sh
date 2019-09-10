eecf779ea065bd4b5868c295f1768a34f7ae2dd8
winters_huang <winters_huang>
2019-09-10 13:57:14 +0800

    /XLX_CN/XLXHost_9999.txt

--- update-XLX9999-Local-Setting.sh
+++ update-XLX9999-Local-Setting.sh
@@ -16,7 +16,8 @@ else
     sudo sed -i '$a\9999;192.168.1.206;4004' /usr/local/etc/XLXHosts.txt.`date +%Y%m%d`
 fi 
 
-sudo sed -i "s/\/home\/pi-star\/XLXHosts9999.txt/\/usr\/local\/etc\/XLXHosts.txt/g" /etc/dmrgateway
+#sudo sed -i "s/\/home\/pi-star\/XLXHosts9999.txt/\/usr\/local\/etc\/XLXHosts.txt/g" /etc/dmrgateway
+sudo sed -i "s/\/usr\/local\/etc\/XLXHosts.txt/\/home\/pi-star\/XLXHosts9999.txt/g" /etc/dmrgateway
 
 sudo grep "XLX_9999" /usr/local/etc/DMR_Hosts.txt > /dev/null
 if [ $? -eq 0 ]; then
